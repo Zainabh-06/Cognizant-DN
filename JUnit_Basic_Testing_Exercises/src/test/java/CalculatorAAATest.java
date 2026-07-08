786a3fa9fd084d9fb2dd82b81dfd450e2844e0fc
Zainabh-06 <zainabh197006@gmail.com>
2026-07-08 14:50:19 +0530

    Added JUnit Basic Testing

---
+++ JUnit_Basic_Testing_Exercises/src/test/java/CalculatorAAATest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorAAATest {
+
+    Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup completed");
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+
+    @After
+    public void tearDown() {
+        calculator = null;
+        System.out.println("Cleanup completed");
+    }
+}
